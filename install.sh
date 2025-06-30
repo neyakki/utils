ecf60801a3434c648a8e54d0155744763c39bd89
neyakki <neyakki@gmail.com>
2025-06-30 15:15:31 +0300

    Уcтановка

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+chmod +x ./bash_scripts/*
+cp -r ./bash_scripts/* $HOME/.local/bin/
+
+for file in ./bash_scripts/*.sh; do
+    cp "$file" "$HOME/.local/bin/$(basename ${file%.sh})"
+done
+
+chmod +x ./python_scripts/*
+for file in ./python_scripts/*.py; do
+    cp "$file" "$HOME/.local/bin/$(basename ${file%.py})"
+done
+
+chmod -x ./bash_scripts/*
+chmod -x ./python_scripts/*
